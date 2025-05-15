48fb6b94f538f15b9b0cac090076d2489738806d
Jun Hyeok Lee <dlwns4194@gmail.com>
2025-05-15 18:27:51 +0900

    init

---
+++ src/main/java/com/guestbooks/GuestbooksApplication.java
@@ -0,0 +1,13 @@
+package com.guestbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestbooksApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GuestbooksApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/guestbooks/GuestbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guestbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbooksApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
